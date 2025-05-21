e30905c8dc636216bc27f373f1bf04c1f71a40ef
Yosef Schmidt <102920177+yosefschmidt@users.noreply.github.com>
2025-05-22 00:43:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { getKodByUrl } = require('./puppeteerScript'); // Import the function from puppeteerScript.js
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
