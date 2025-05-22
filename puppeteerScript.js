d71f2ec4a06f6d904e016fa25e008f0ad7a230af
Yosef Schmidt <102920177+yosefschmidt@users.noreply.github.com>
2025-05-22 10:03:08 +0300

    Update puppeteerScript.js

--- puppeteerScript.js
+++ puppeteerScript.js
@@ -58,7 +58,7 @@ async function getKodByUrl(data) {
 async function processData(v, i, benefitUrl, phoneNumber,email) {
   const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
  const browser = await puppeteer.launch({
-   executablePath: '/usr/bin/google-chrome',
+   executablePath: '/usr/bin/google-chrome-stable',
   headless: "new",
   args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
